0d9bbd943b84a21accfeae8f3228c26e27d2e08e
Didrikhya <uzirdidrikhya@gmail.com>
2023-10-29 15:42:52 +0530

    fixed the searchbar

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { useGlobalContext } from '../context/global'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Sidebar from './Sidebar'
 
 
-function Popular(){
-    const {popularAnime, isSearch} = useGlobalContext()
+function Popular({rendered}){
+    const {popularAnime, isSearch, searchResults} = useGlobalContext()
 
     const conditionalRender = () => {
-        if(!isSearch){
-            return popularAnime.map((anime) => {
+        if(!isSearch && rendered === 'popular'){
+            return popularAnime?.map((anime) => {
                 return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
                     <img src={anime.images.jpg.large_image_url} alt=""/>
+                </Link>
+            })
+        }else{
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
                 </Link>
             })
         }
